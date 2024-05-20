7e9d2329c2a17a4192e9853b0bfe51a204e51e9f
jingon0 <168407337+jingon0@users.noreply.github.com>
2024-05-20 12:12:39 +0900

    Add files via upload

---
+++ 수학공식/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 수학공식;
+internal class TesT
+{
+    static void Main(string[] args)
+    {
+        string num = Console.ReadLine();
+
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int d = 1;
+
+        for (int i = 0; i < int.Parse(num); i++)
+        {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+
+            Console.WriteLine(c + " ");
+        }
+    }
+}
